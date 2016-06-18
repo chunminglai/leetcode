0a1844af81f7faf936a5b09ea50b22fab7aa19b4
Chun-Ming Lai <cmlaih@gmail.com>
2016-06-18 01:03:55 -0700

    Create power_of_two_231.java

---
+++ power_of_two_231.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if ((n & (n-1)) == 0 && n > 0)
+            return true;
+        else 
+            return false;
+    }
+}
